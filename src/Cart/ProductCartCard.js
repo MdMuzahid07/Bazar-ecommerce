9f04421a219ee8bc56f93a64fb4f56721007ab96
MdMuzahid07 <mdmuzahid7396@gmail.com>
2023-04-06 11:29:16 +0600

    sidebarCartCard component maked ofr SidebarCart

--- src/Cart/ProductCartCard.js
+++ src/Cart/ProductCartCard.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const ProductCartCard = () => {
     return (
-        <section>
-
+        <section className="my-3">
+            <div className="grid grid-cols-5 gap-3 mb-3">
+                <div className="col-span-1">
+                    <button className="w-8 h-8 border border-red-500 rounded-full font-bold">+</button>
+                    <div className="w-8"><p className="text-center">0</p></div>
+                    <button className="w-8 h-8 border border-red-500 rounded-full font-bold">-</button>
+                </div>
+                <div className="col-span-4">
+                    <div>
+                        <div className="flex items-center w-full">
+                            <div>
+                                <img className="w-20" src="https://bazaar.ui-lib.com/assets/images/products/Fashion/Clothes/1.SilverHighNeckSweater.png" alt="product_img" />
+                            </div>
+                            <div>
+                                <p className="text-sm">Sliver High Neck Swi...</p>
+                                <p className="text-xs"><small>$210 x 2</small></p>
+                                <p className="text-sm">$420</p>
+                            </div>
+                            <AiOutlineClose style={{ fontSize: "20px", marginLeft: "10  px" }} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr />
         </section>
     );
 };
--- src/Cart/SidebarCart.js
+++ src/Cart/SidebarCart.js
@@ -5,13 +5,15 @@ import 'animate.css';
 
 const SidebarCart = ({ isCartOpen, toggleSidebarCart }) => {
     return (
-        <section className="min-h-screen w-full lg:w-72 xl:w-96 bg-white drop-shadow fixed top-0 right-0 z-50 p-7 animate__animated animate__bounceInRight">
+        <section className="min-h-screen w-full lg:w-72 xl:w-96 bg-white drop-shadow fixed top-0 right-0 z-50 p-7 animate__animated animate__slideInRight">
             <div className="flex items-center justify-between mb-3">
                 <p className="text-lg flex items-center"><AiOutlineShopping style={{ fontSize: "25px", marginRight: "5px" }} /> <span><small>3 items</small></span></p>
                 <button onClick={toggleSidebarCart} className="btn btn-sm rounded-none"><AiOutlineClose /></button>
             </div>
             <hr />
             <div className="mt-2">
+                <ProductCartCard />
+                <ProductCartCard />
                 <ProductCartCard />
             </div>
             <div>
