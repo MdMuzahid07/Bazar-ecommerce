1a1e6101e10df4909c93c77ff2e057a4c698ee01
MdMuzahid07 <mdmuzahid7396@gmail.com>
2023-04-06 12:00:11 +0600

    SidebarCart, and ProductCartCard component UI complete

--- src/Cart/SidebarCart.js
+++ src/Cart/SidebarCart.js
@@ -15,7 +15,7 @@ const SidebarCart = ({ isCartOpen, toggleSidebarCart }) => {
     ];
 
     return (
-        <section className="min-h-screen w-full lg:w-72 xl:w-96 bg-white drop-shadow fixed top-0 right-0 z-50 p-7 animate__animated animate__slideInRight">
+        <section className="min-h-screen w-full lg:w-72 xl:w-96 bg-white drop-shadow fixed top-0 right-0 z-50 p-5 animate__animated animate__slideInRight">
             <div className="flex items-center justify-between mb-3">
                 <p className="text-lg flex items-center"><AiOutlineShopping style={{ fontSize: "25px", marginRight: "5px" }} /> <span><small>3 items</small></span></p>
                 <button onClick={toggleSidebarCart} className="btn btn-sm rounded-none"><AiOutlineClose /></button>
@@ -26,10 +26,11 @@ const SidebarCart = ({ isCartOpen, toggleSidebarCart }) => {
                     product?.map((product) => <ProductCartCard key={product._id} product={product} />)
                 }
             </div>
-            <div>
-                <div className="absolute bottom-0">
-                    <button className="border">view cart</button>
-                    <button className="border">checkout</button>
+            <div className="flex justify-center">
+                <div className="absolute bottom-5">
+                    <button className="border bg-red-500 text-white p-1 w-80 mb-3">Checkout now</button>
+                    <br />
+                    <button className="border border-red-500 p-1 w-80 font-bold text-red-500">View Cart</button>
                 </div>
             </div>
         </section>
