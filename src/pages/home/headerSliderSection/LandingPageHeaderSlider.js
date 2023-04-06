e9cdc1126b7b69fda0ac279710b49b7960cd8e06
MdMuzahid07 <mdmuzahid7396@gmail.com>
2023-04-06 16:14:41 +0600

    headerSliderSection's slider maked responsive friendly

--- src/pages/home/headerSliderSection/LandingPageHeaderSlider.js
+++ src/pages/home/headerSliderSection/LandingPageHeaderSlider.js
@@ -20,13 +20,13 @@ const LandingPageHeaderSlider = () => {
                 className="mySwiper"
             >
                 <SwiperSlide>
-                    <div className="rounded bg-gradient-to-r from-yellow-50 p-24">
+                    <div className="rounded bg-gradient-to-r from-yellow-50 p-10 sm:p-20 md:p-24">
                         <div className="grid grid-cols-1 md:grid-cols-2 md:gap-5">
                             <div>
                                 <div>
-                                    <h1 className="text-2xl mb-2">LIFESTYLE COLLECTION</h1>
-                                    <h2 className="text-5xl font-bold mb-2">MEN</h2>
-                                    <h2 className="text-3xl mb-2">Sale UP TO <span className="text-red-500">30% OFF</span></h2>
+                                    <h1 className="text-xl md:text-2xl mb-2">LIFESTYLE COLLECTION</h1>
+                                    <h2 className="text-2xl md:text-5xl font-bold mb-2">MEN</h2>
+                                    <h2 className="text-xl   md:text-3xl mb-2">Sale UP TO <span className="text-red-500">30% OFF</span></h2>
                                     <h3 className="text-base mb-10">Get Free Shipping On Orders Over $99.00</h3>
                                 </div>
                                 <button className="px-7 py-2 bg-black text-white text-sm font-bold">Shop Now</button>
@@ -37,14 +37,15 @@ const LandingPageHeaderSlider = () => {
                         </div>
                     </div>
                 </SwiperSlide>
+
                 <SwiperSlide>
-                    <div className="rounded bg-gradient-to-r from-yellow-50 p-24">
+                    <div className="rounded bg-gradient-to-r from-yellow-50 p-10 sm:p-20 md:p-24">
                         <div className="grid grid-cols-1 md:grid-cols-2 md:gap-5">
                             <div>
                                 <div>
-                                    <h1 className="text-2xl mb-2">LIFESTYLE COLLECTION</h1>
-                                    <h2 className="text-5xl font-bold mb-2">MEN</h2>
-                                    <h2 className="text-3xl mb-2">Sale UP TO <span className="text-red-500">30% OFF</span></h2>
+                                    <h1 className="text-xl md:text-2xl mb-2">LIFESTYLE COLLECTION</h1>
+                                    <h2 className="text-2xl md:text-5xl font-bold mb-2">MEN</h2>
+                                    <h2 className="text-xl   md:text-3xl mb-2">Sale UP TO <span className="text-red-500">30% OFF</span></h2>
                                     <h3 className="text-base mb-10">Get Free Shipping On Orders Over $99.00</h3>
                                 </div>
                                 <button className="px-7 py-2 bg-black text-white text-sm font-bold">Shop Now</button>
@@ -55,14 +56,16 @@ const LandingPageHeaderSlider = () => {
                         </div>
                     </div>
                 </SwiperSlide>
+
+
                 <SwiperSlide>
-                    <div className="rounded bg-gradient-to-r from-yellow-50 p-24">
+                    <div className="rounded bg-gradient-to-r from-yellow-50 p-10 sm:p-20 md:p-24">
                         <div className="grid grid-cols-1 md:grid-cols-2 md:gap-5">
                             <div>
                                 <div>
-                                    <h1 className="text-2xl mb-2">LIFESTYLE COLLECTION</h1>
-                                    <h2 className="text-5xl font-bold mb-2">MEN</h2>
-                                    <h2 className="text-3xl mb-2">Sale UP TO <span className="text-red-500">30% OFF</span></h2>
+                                    <h1 className="text-xl md:text-2xl mb-2">LIFESTYLE COLLECTION</h1>
+                                    <h2 className="text-2xl md:text-5xl font-bold mb-2">MEN</h2>
+                                    <h2 className="text-xl   md:text-3xl mb-2">Sale UP TO <span className="text-red-500">30% OFF</span></h2>
                                     <h3 className="text-base mb-10">Get Free Shipping On Orders Over $99.00</h3>
                                 </div>
                                 <button className="px-7 py-2 bg-black text-white text-sm font-bold">Shop Now</button>
