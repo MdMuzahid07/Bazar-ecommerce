850b45dabf9c809459f979feab7f00289eb0a6f9
MdMuzahid07 <mdmuzahid7396@gmail.com>
2023-04-07 16:06:05 +0600

    working on AccountCreateModalForm component

--- src/components/AccountCreateModalForm/AccountCreateModalForm.js
+++ src/components/AccountCreateModalForm/AccountCreateModalForm.js
@@ -1,12 +1,17 @@
 const AccountCreateModalForm = ({ isOpen, handleOverlayClick, handleCloseModal }) => {
 
+    const handleOnSubmit = (event) => {
+        event.preventDefault();
+        window.alert("btn onsubmit check")
+    };
+
     return (
         <>
             <div
                 className={`fixed inset-0 z-50 ${isOpen ? 'flex' : 'hidden'}`}
                 onClick={handleOverlayClick}
             >
-                <div className="relative m-auto h-screen w-full md:w-96 md:h-96 overflow-auto bg-white md:rounded-lg shadow-lg">
+                <div className="relative m-auto w-full max-w-sm px-10 py-7 overflow-auto bg-white md:rounded-lg shadow-lg">
                     <div className="absolute top-0 right-0 p-4">
                         <button onClick={handleCloseModal}>
                             <svg viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6">
@@ -18,12 +23,33 @@ const AccountCreateModalForm = ({ isOpen, handleOverlayClick, handleCloseModal }
                             </svg>
                         </button>
                     </div>
-                    <div className="p-6">
-                        <h2 className="text-xl font-bold mb-4">Modal Title</h2>
-                        <p className="mb-4">Modal content goes here.</p>
-                        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" onClick={handleCloseModal}>
-                            Close
-                        </button>
+                    <div >
+                        <div>
+                            <div className="flex justify-center">
+                                <img src="https://bazaar.ui-lib.com/assets/images/bazaar-black-sm.svg" alt="brand_logo" />
+                            </div>
+                            <p className="text-center mt-3">Welcome To Bazaar</p>
+                        </div>
+                        <div className="mt-5">
+
+                            <div>
+                                <form onSubmit={handleOnSubmit}>
+                                    <div className="mb-2">
+                                        <label htmlFor="email"><small>Email or Phone Number</small></label>
+                                        <input className="border px-3 py-2 w-full rounded" type="text" id="email" placeholder="example@gmail.com" />
+                                    </div>
+                                    <div className="mb-5">
+                                        <label htmlFor="password"><small>Password</small></label>
+                                        <input className="border px-3 py-2 w-full rounded" type="password" id="password" placeholder="##########" />
+                                    </div>
+                                    <button type="submit" className="p-2 bg-red-500 text-white w-full rounded ">Login</button>
+                                </form>
+                            </div>
+                            <hr className="my-5" />
+                            <div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
