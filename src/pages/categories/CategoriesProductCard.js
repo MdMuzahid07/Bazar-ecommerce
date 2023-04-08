e6d517ececa439aad396d2055391dd07c2430de3
MdMuzahid07 <mdmuzahid7396@gmail.com>
2023-04-08 18:01:12 +0600

    CategoriesProductCard component of Categories page, UI complete with button increment and decrement conditional hide minus button

--- src/pages/categories/CategoriesProductCard.js
+++ src/pages/categories/CategoriesProductCard.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import { AiOutlineStar } from "react-icons/ai";
 
 const CategoriesProductCard = () => {
-    const [quantity, setQuantity] = useState(1);
+    const [quantity, setQuantity] = useState(0);
 
     const incrementQuantity = () => {
         setQuantity(quantity + 1);
     };
 
     const decrementQuantity = () => {
-        if (quantity > 1) {
+        if (quantity > 0) {
             setQuantity(quantity - 1);
         }
     };
@@ -26,10 +26,18 @@ const CategoriesProductCard = () => {
                     <AiOutlineStar style={{ fontSize: "15px" }} />
                     <AiOutlineStar style={{ fontSize: "15px" }} />
                 </div>
-                <div className="flex items-center justify-between">
+                <div className="flex items-center justify-between relative">
                     <p className="text-red-500"><small>$187.00</small></p>
-                    <button className="p-2 border border-red text-red-500 rounded">-</button>
-                    <button className="p-2 border border-red text-red-500 rounded">+</button>
+                    <div className="absolute bottom-0 right-0">
+                        {
+                            (quantity > 0) ? <button onClick={decrementQuantity} className="px-2 w-7 h-7 border border-red text-red-500 rounded">-</button> : ""
+                        }
+                        {
+
+                            (quantity > 0) ? <p className="py-2 text-center">{quantity}</p> : ""
+                        }
+                        <button onClick={incrementQuantity} className="px-2 border border-red text-red-500 rounded">+</button>
+                    </div>
                 </div>
             </div>
         </div>
