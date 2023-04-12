7e127e55e7474714fe275e9c5bff60fcf8894e4e
MdMuzahid07 <mdmuzahid7396@gmail.com>
2023-04-12 14:12:57 +0600

    working on order track page

--- src/pages/vendor/OrderTrackPage.js
+++ src/pages/vendor/OrderTrackPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { GoPackage } from 'react-icons/go';
+import { BsFillChatLeftTextFill } from 'react-icons/bs';
+import { GiBoxUnpacking } from 'react-icons/gi';
 
 const OrderTrackPage = () => {
     return (
         <section className="max-w-6xl mx-auto">
-            <div className="flex flex-wrap items-center justify-between mt-10 bg-white p-3">
+            <div className="flex flex-wrap items-center justify-between mt-10 bg-white p-4">
                 <div>
                     <h3>Order #985987689598765</h3>
                     <p><small>Placed on 10 Mar 2023 21:32:26</small></p>
@@ -12,6 +15,43 @@ const OrderTrackPage = () => {
                     <h2 className="text-lg"><span className="text-slate-400 font-bold">Total</span> : $185</h2>
                 </div>
             </div>
+
+            {/* package section start here */}
+
+            <div className="bg-white p-4 mt-5">
+
+                <div className="flex items-center justify-between">
+                    <div>
+                        <div className="flex items-center gap-2">
+                            <GoPackage />
+                            <p>Package 1</p>
+                        </div>
+                        <p><small>Sold by: <span className="text-green-500 font-bold">Daraz Mall</span></small></p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <BsFillChatLeftTextFill />
+                        <p><small>Chat Now</small></p>
+                    </div>
+                </div>
+
+                <hr className="my-3" />
+
+                <div>
+                    <div className="flex flex-wrap items-center justify-between">
+                        <p>Delivered on 1 Mar 2023</p>
+                        <p className="flex items-center"><GiBoxUnpacking /> Standard Delivery</p>
+                    </div>
+
+                    <div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            {/* package section end here */}
+
         </section>
     );
 };
